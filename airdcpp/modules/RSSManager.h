51baf3e4c742a2d28f1271830461f54d933a03f5
maksis <maksis@adrenaline-network.com>
2026-09-10 12:29:51 +0300

    Squashed 'airdcpp-core/' changes from c135b79cc..de3ce7ead
    de3ce7ead RSS feeds: undeclared identifier fixed
    78154962c "Require an exact match" has got an option in the RSS Feeds. Compile warning "right shift by too large amount, data loss" removed.
    bb8e729e2 Hub: access IP addresses directly from HubSettings
    55d51ceb8 Clang: fix a few build warnings
    
    git-subtree-dir: airdcpp-core
    git-subtree-split: de3ce7ead9fa340f192ee87862bbf695d848c28e

--- airdcpp/modules/RSSManager.h
+++ airdcpp/modules/RSSManager.h
@@ -50,9 +50,9 @@ typedef std::shared_ptr<RSSData> RSSDataPtr;
 class RSSFilter : public StringMatch {
 public:
 
-	RSSFilter(const string& aFilterPattern, const string& aDownloadTarget, int aMethod, const string& aGroup, bool aSkipDupes, int aAction, int aExpireDays, bool aFormatTime) noexcept :
+	RSSFilter(const string& aFilterPattern, const string& aDownloadTarget, int aMethod, const string& aGroup, bool aSkipDupes, int aAction, int aExpireDays, bool aFormatTime, bool aAsExactMatch = true) noexcept :
 		filterPattern(aFilterPattern), downloadTarget(aDownloadTarget), autosearchGroup(aGroup), skipDupes(aSkipDupes), filterAction(aAction), expireDays(aExpireDays),
-		formatTimeParams(aFormatTime)
+		formatTimeParams(aFormatTime), asExactMatch(aAsExactMatch)
 	{
 		pattern = aFilterPattern;
 		setMethod((StringMatch::Method)aMethod);
@@ -66,6 +66,8 @@ public:
 	IGETSET(int, filterAction, FilterAction, DOWNLOAD);
 	IGETSET(int, expireDays, ExpireDays, 3);
 	IGETSET(bool, formatTimeParams, FormatTimeParams, false);
+	// Auto-search match type for spawned auto searches: true = exact (default), false = partial
+	IGETSET(bool, asExactMatch, AsExactMatch, true);
 
 	bool skipDupes = true;
 
